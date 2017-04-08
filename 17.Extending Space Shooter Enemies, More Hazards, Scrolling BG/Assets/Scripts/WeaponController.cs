9df20682c549d05edf0e106a45a4b74f626510c9
tanapon <tanapon>
2017-04-08 13:33:54 +0700

    17.Extending Space Shooter Enemies, More Hazards, Scrolling BG...
    ทำให้อุกาบาตออกมาแบบสุ่ม น้อย มาก

---
+++ 17.Extending Space Shooter Enemies, More Hazards, Scrolling BG/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	public float delay;
+	public AudioClip[] clips;
+
+	private AudioSource audioSource;
+
+	void Start ()
+	{
+		audioSource = GetComponent<AudioSource> ();
+		InvokeRepeating ("Fire", delay, fireRate);
+	}
+
+	void Fire ()
+	{
+		Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		AudioClip clip = clips [Random.Range (0, clips.Length)];
+		audioSource.clip = clip;
+		audioSource.Play ();
+	}
+}
